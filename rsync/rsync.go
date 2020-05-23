90576b74228e5060d8720e924b7156ebbeca9b51
Chen Lixiang <lixiang3608@outlook.com>
2020-05-23 21:22:19 +0800

    fix path, fix tests

--- rsync/rsync.go
+++ rsync/rsync.go
@@ -12,21 +12,17 @@ type Config struct {
 	Command   string // set Command as `rsync` on default
 	LocalDir  string
 	RemoteDir string
+	Upstream  string
 	Args      []string
 }
 
-// TimeoutCallback represents timeout call back function for rsync command
-type TimeoutCallback func(*Config)
-
-// ErrorCallback represents callback function when
-type ErrorCallback func(*Config, error)
-
 // InitConfig init and return a rsync task config
-func InitConfig(localDir, remoteDir string, args []string) *Config {
+func InitConfig(localDir, remoteDir, upstream string, args []string) *Config {
 	return &Config{
 		Command:   RSYNC_CMD,
 		LocalDir:  localDir,
 		RemoteDir: remoteDir,
+		Upstream:  upstream,
 		Args:      args,
 	}
 }
@@ -45,7 +41,9 @@ func CheckRsync() (hasRsync bool, info string) {
 
 // ExecCommand start calling rsync command with specified config
 func ExecCommand(conf *Config) error {
-	rsyncCmd := exec.Command(conf.Command, conf.Args...)
+	args := append(conf.Args, "rsync://"+conf.Upstream+conf.RemoteDir, "./")
+	rsyncCmd := exec.Command(conf.Command, args...)
+	rsyncCmd.Dir = conf.LocalDir
 	if err := rsyncCmd.Run(); err != nil {
 		return err
 	}
--- rsync/rsync_test.go
+++ rsync/rsync_test.go
@@ -1,6 +1,7 @@
 package rsync
 
 import (
+	"os"
 	"testing"
 )
 
@@ -10,4 +11,13 @@ func TestRsyncCheck(t *testing.T) {
 		t.Errorf("rsync is not installed on this machine, stop.")
 	}
 	t.Logf("check rsync ok. version info: \n%s", info)
+}
+
+func TestExecRsync(t *testing.T) {
+	os.Mkdir("test_rsync", os.ModePerm)
+	defer os.RemoveAll("test_rsync")
+	conf := InitConfig("./test_rsync", "/eclipse/swtchart/releases/0.12.0/release/", "mirrors.tuna.tsinghua.edu.cn", []string{"-avz", "--delete"})
+	if err := ExecCommand(conf); err != nil {
+		t.Error(err)
+	}
 }
